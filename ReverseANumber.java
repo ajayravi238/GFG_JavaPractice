b5e863a46cf1b1146ed4c98a7ed13717806447c6
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-02-07 10:25:33 +0530

    Create Reverse A Number

---
+++ ReverseANumber.java
@@ -0,0 +1,20 @@
+// Java Program to Find Reverse a Number
+
+import java.util.Scanner;
+
+public class ReverseANumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int reverse = 0;
+		
+		while(num > 0) {
+			int rem = num%10;
+			reverse = reverse * 10 + rem;
+			num = num / 10;
+		}
+		System.out.println(reverse);
+	}
+
+}
