4a17c534b649cc35b4cd12b8f7075adb6039ddfb
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-02-08 19:58:43 +0530

    Create Reverse A String

---
+++ ReverseAString.java
@@ -0,0 +1,16 @@
+// Reverse a String in Java
+
+import java.util.Scanner;
+
+public class ReverseAString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		for(int i=str.length()-1; i>=0; i--) {
+			System.out.print(str.charAt(i));
+		}
+	}
+
+}
