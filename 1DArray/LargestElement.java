dc713f6b7cd0c5197dbdec3c588bd6cfb6827637
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-02-13 18:56:00 +0530

    Create Largest Element

---
+++ 1DArray/LargestElement.java
@@ -0,0 +1,26 @@
+package gfgJavaPractice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LargestElement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length-1; j++) {
+				if(arr[j] < arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		System.out.println(arr[0]);
+	}
+}
