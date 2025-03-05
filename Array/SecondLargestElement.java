ae204507dd2ee5b619c685af605cfe4ed5a4af36
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-03-05 21:56:36 +0530

    Create Second Largest Element

---
+++ Array/SecondLargestElement.java
@@ -0,0 +1,25 @@
+// Second Largest Element in an Array
+
+import java.util.Scanner;
+
+public class SecondLargestElement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] arr = new int[num];
+		for(int i=0; i<num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<num-1; j++) {
+				if(arr[j] < arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		System.out.println(arr[1]);
+	}
+}
